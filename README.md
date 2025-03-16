383c4a8ca6a94a79c4574716b91f22fa85e99852
Brandon Jutras <jutrasb@gmail.com>
2025-03-16 19:32:45 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# Pi Video Looper
+
+A set of scripts to allow the Raspberry Pi lite operating system to automatically mount USB mass storage devices and play videos using VLC media player.
+
+Based on information found at https://github.com/term7/RaspberryPi-VLC_Videolooper
+
+## Prerequisites
+
+Update the operating system:
+```
+sudo apt-get update && sudo apt-get upgrade -y
+```
+
+Install required packages:
+```
+sudo apt-get install exfat-fuse vlc -y
+```
+
+## Installation
+
+1. Install `udev-media-automount` from [here](https://github.com/Ferk/udev-media-automount). After installation, USB mass storage devices will automatically be mounted to the `/media` folder.
+1. Copy the VLC autoplay service `autoplay.service` from this repository to `/lib/systemd/system/autoplay.service`.
+```
+sudo cp ~/pi-video-looper/files/autoplay.service /lib/systemd/system/autoplay.service
+```
+1. Create the `scripts` folder.
+```
+mkdir ~/scripts
+```
+1. Copy the autoplay script `autoplay.sh` from this repository to `~/scripts/autoplay.sh`.
+1. Enable the VLC autoplay service
+```
+sudo systemctl enable autoplay.service
+```
+1. Attach a USB mass storage device and ensure it is mounted to `/media`
+1. Reboot the Pi.
+```
+sudo reboot
+```
---
+++ files/autoplay.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# VLC OPTIONS:
+# View all possible options: vlc -H
+
+export XDG_RUNTIME_DIR=/run/user/1000
+
+#export AUTOPLAY=/home/pi/videos/autoplay
+export USB=/media/
+export PLAYLIST=/home/pi/playlist.m3u
+
+# Video Filetypes (you can add more filetypes):
+
+FILETYPES="-iname *.mp4 -o -iname *.mov -o -iname *.mkv"
+
+# Playlist Options:
+Playlist_Options="-L --started-from-file --one-instance --playlist-enqueue"
+
+# Output Modules (edit to add options):
+Video_Output=""
+
+# Audio Options:
+Audio_Output="--stereo-mode 1"
+
+# Interface Options:
+Interface_Options="-f --loop --no-video-title-show"
+
+
+# Create Playlist File
+# COMMENT: change sleep to 3 if you only want to play from the internal disk to start playback earlier, 25 is only necessary in order to find the USB drive after the Pi boots up, because otherwise the script may start before the USB drive is mounted and no files will be added from USB!
+
+sleep 25
+# Create Playlist File
+echo "#EXTM3U" > "$PLAYLIST"
+#find "$AUTOPLAY" ! -iname ".*" -type f \( $FILETYPES \) 2>/dev/null >> "$PLAYLIST"
+find "$USB" ! -iname ".*" -type f \( $FILETYPES \) 2>/dev/null >> "$PLAYLIST"
+
+# Play Playlist if Files Exist
+if [ -s "$PLAYLIST" ]; then
+   /usr/bin/vlc -I dummy -q $Video_Output $Audio_Output $Interface_Options $Playlist_Options "$PLAYLIST"
+else
+   echo "No files found to play."
+fi
